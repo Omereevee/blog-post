d8e39752736f84555cbef5170e6c3d49856290d3
Your Name <omerkumble@gmail.com>
2026-09-12 17:48:58 -0700

    contact me

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import smtplib
 
+mail="testuserbob28@gmail.com"
+password = 'senx stbb ryos vjnd'
 
 app = Flask(__name__)
 
@@ -16,9 +19,27 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact',methods=["GET","POST"])
 def contact():
-    return render_template('contact.html')
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        message = request.form["message"]
+
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=mail, password=password)
+            connection.sendmail(
+                from_addr=email,
+                to_addrs="omerkumble@gmail.com",
+                msg=f"{message}"
+            )
+
+        return render_template('contact.html', Title="Sent Successfully", maincontent= False)
+
+    else:
+        return render_template('contact.html', Title="Contact me", maincontent= True)
 
 @app.route('/post/<int:id>')
 def post(id):
